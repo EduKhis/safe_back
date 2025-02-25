dd11fa24e38272dadf207bf77c2a543a5a5b827f
e.khisamutdinov <e.khisamutdinov@ngrsoftlab.ru>
2025-02-25 17:14:24 +0300

    set field

--- src/main/java/com/example/demo/SafetyBackApplication.java
+++ src/main/java/com/example/demo/SafetyBackApplication.java
@@ -3,11 +3,25 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class SafetyBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SafetyBackApplication.class, args);
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("server", "8080");
+        properties.put("serveri", "8080");
+
+        Map<String, String> properties2 = new HashMap<>();
+        properties2.put("server4", "8082");
+
+        properties.putAll(properties2);
+
+        System.out.println(properties);
     }
 
 }
