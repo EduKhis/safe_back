cb6eee6a7009536ac9f1bea955f1104c9f34be7f
e.khisamutdinov <e.khisamutdinov@ngrsoftlab.ru>
2025-03-03 15:13:19 +0300

    set field

--- src/main/java/com/example/demo/mapper/RiskMapper.java
+++ src/main/java/com/example/demo/mapper/RiskMapper.java
@@ -38,6 +38,7 @@ public class RiskMapper {
 
         List<RiskContentDto> risks = new ArrayList<>();
         for (Risk risk : risksR) {
+            System.out.println(risk.getPhotosList().isEmpty() ? null : risk.getPhotosList().get(0));
             risks.add(RiskContentDto.builder()
                     .photo(risk.getPhotosList().isEmpty() ? null : risk.getPhotosList().get(0))
                     .id(risk.getId())
